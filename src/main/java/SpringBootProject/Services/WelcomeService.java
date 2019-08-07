f397a8ff1d87555ec3a20340366c5bc1705d48ce
handerson5991 <handerson5991@gmail.com>
2019-08-06 21:09:44 -0500

    initial commit; Created Java Spring and Swagger dependencies.

---
+++ src/main/java/SpringBootProject/Services/WelcomeService.java
@@ -0,0 +1,13 @@
+package SpringBootProject.Services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+public class WelcomeService {
+
+    @GetMapping("/")
+    public String retrieveWelcomeMessage() {
+        return "Hello again world!";
+    }
+}
